b5d86d5187234cddf9537e5d4fbb88044305834c
James Danielson <geekylink@geekylink.github.io>
2022-04-15 19:22:55 -0400

    added an example

---
+++ code.asm
@@ -0,0 +1,11 @@
+;;; Generated with GeekyLink's BMP to Atari 2600 Graphics compiler
+;;; http://www.gekinzuku.com
+PlayerGraphic
+	.byte #%01000100
+	.byte #%01000100
+	.byte #%01000100
+	.byte #%01111100
+	.byte #%01000100
+	.byte #%01000100
+	.byte #%00101000
+	.byte #%00010000
